d43752d7ae0de001c6916d604e55398f358899ef
Olha Mushket <olhamushket@gmail.com>
2022-07-14 18:23:48 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
@@ -5,33 +5,106 @@ import java.util.StringJoiner;
 public class Expressions {
 
     public static Variable var(String name, int value) {
-        //implement this method
-        return null;
+        return new Variable(name, value);
     }
 
     public static Expression val(int value) {
-        //implement this method
-        return null;
+        Expression exp = new Expression() {
+            @Override
+            public int evaluate() {
+                return value;
+            }
+
+            @Override
+            public String toExpressionString() {
+                if (value<0)
+                    return "("+value+")";
+                else
+                    return Integer.toString(value);
+            }
+        };
+        return exp;
     }
 
     public static Expression sum(Expression... members){
-        //implement this method
-        return null;
+        Expression exp =new Expression() {
+            @Override
+            public int evaluate() {
+                int sum=0;
+                for (Expression member: members) {
+                    sum+=member.evaluate();
+                }
+                return sum;
+            }
+
+            @Override
+            public String toExpressionString() {
+                StringBuilder s=new StringBuilder();
+                s.append("(");
+                for (int i = 0; i < members.length; i++) {
+                    s.append(members[i].toExpressionString());
+                    if (i!= members.length-1)
+                        s.append(" + ");
+                }
+                s.append(")");
+                return s.toString();
+            }
+        };
+        return exp;
     }
 
     public static Expression product(Expression... members){
-        //implement this method
-        return null;
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                int sum=1;
+                for (Expression member: members) {
+                    sum*=member.evaluate();
+                }
+                return sum;
+            }
+
+            @Override
+            public String toExpressionString() {
+                StringBuilder s=new StringBuilder();
+                s.append("(");
+                for (int i = 0; i < members.length; i++) {
+                    s.append(members[i].toExpressionString());
+                    if (i!= members.length-1)
+                        s.append(" * ");
+                }
+                s.append(")");
+                return s.toString();
+            }
+        };
     }
 
     public static Expression difference(Expression minuend, Expression subtrahend){
-        //implement this method
-        return null;
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                return minuend.evaluate()-subtrahend.evaluate();
+            }
+
+            @Override
+            public String toExpressionString() {
+                return "("+minuend.toExpressionString()+" - "+subtrahend.toExpressionString()+")";
+            }
+        };
     }
 
     public static Expression fraction(Expression dividend, Expression divisor){
-        //implement this method
-        return null;
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                return dividend.evaluate()/divisor.evaluate();
+            }
+
+            @Override
+            public String toExpressionString() {
+                return "("+dividend.toExpressionString()+" / "+divisor.toExpressionString()+")";
+            }
+        };
     }
 
 }
